e4182ad9230e4ec5329beaa5118e56f8c72406ae
chaostalker <157380260+chaostalker@users.noreply.github.com>
2024-01-22 20:43:39 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+var util = require("util");
+var http = require("http");
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
+util.log("Server running at https://localhost:8080/");
